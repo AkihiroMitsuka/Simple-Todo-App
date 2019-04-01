fd9240795d963131d1b7149bdc531460c378700c
Akihiro Mitsuka <akihiro.mitsuka@gmail.com>
2019-04-01 23:16:48 +0900

    Task Add Create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
     @task = Task.all
   end
 
+  # route /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # TaskCreate
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  # Strong Parameters field
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
+
 end
